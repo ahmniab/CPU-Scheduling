dda6d44aaec59e8fdbc925d368db63e7a62edeab
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-04-01 13:37:19 +0200

    add main files

---
+++ src/main/java/com/cpu_scheduling/cpu/scheduling/CPUScheduling.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cpu_scheduling.cpu.scheduling;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class CPUScheduling {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
