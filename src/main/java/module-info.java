233d5b54e5c3c5ae76ce5aba2d0e5156caccb292
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-04-16 21:51:59 +0200

    create table

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module os.team.cpuscheduling {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens os.team.cpuscheduling to javafx.fxml;
-    exports os.team.cpuscheduling;
-}
