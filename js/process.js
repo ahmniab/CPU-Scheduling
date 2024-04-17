1a66ad1a5534bc643db99c02742a73bf712ba29c
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-04-17 11:17:53 +0200

    modify process.js

---
+++ js/process.js
@@ -0,0 +1,29 @@
+class Process {
+    constructor(pid , AT, BT, WT, TAT, RT) {
+      this.pid = pid;
+      this.AT = AT;
+      this.BT = BT;
+      this.WT = WT;
+      this.TAT = TAT;
+      this.RT = RT;
+    }
+  }
+  
+  var Processes = new Array();
+
+  function extract_processes() {
+    var table = document.querySelector(".container .inputs .table table");
+    var rows = table.querySelectorAll("tr");
+    for (let i = 1; i < rows.length; i++) {
+
+     var row  = rows[i].querySelectorAll("td");
+     Processes.push(new Process(Number(row[0].innerText),Number(row[1].innerText),Number(row[2].innerText),Number(row[3].innerText),Number(row[4].innerText),Number(row[5].innerText)));
+      
+    }
+    
+  }
+  extract_processes();
+  Processes.forEach(element => {
+    console.log(element);
+    
+  });
