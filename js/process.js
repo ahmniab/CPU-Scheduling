d76bb8d58ad18c9e35b06c89f5e9c2cf62fd1e1b
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-04-17 19:37:07 +0200

    Update process.js

--- js/process.js
+++ js/process.js
@@ -7,23 +7,28 @@ class Process {
       this.TAT = TAT;
       this.RT = RT;
     }
+}
+  
+var Processes = new Array();
+
+function extract_processes() {
+  var table = document.querySelector(".container .inputs .table table");
+  var rows = table.querySelectorAll("tr");
+  for (let i = 1; i < rows.length; i++) {
+
+    var row  = rows[i].querySelectorAll("td");
+    Processes.push(new Process(Number(row[0].innerText),Number(row[1].innerText),Number(row[2].innerText),Number(row[3].innerText),Number(row[4].innerText),Number(row[5].innerText)));
+    
   }
   
-  var Processes = new Array();
+}
 
-  function extract_processes() {
-    var table = document.querySelector(".container .inputs .table table");
-    var rows = table.querySelectorAll("tr");
-    for (let i = 1; i < rows.length; i++) {
+function aval_processes(Processes , time) {
+  
+}
 
-     var row  = rows[i].querySelectorAll("td");
-     Processes.push(new Process(Number(row[0].innerText),Number(row[1].innerText),Number(row[2].innerText),Number(row[3].innerText),Number(row[4].innerText),Number(row[5].innerText)));
-      
-    }
-    
-  }
-  extract_processes();
-  Processes.forEach(element => {
-    console.log(element);
-    
-  });
+extract_processes();
+Processes.forEach(element => {
+  console.log(element);
+  
+});
